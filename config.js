b516ed17766502289ee813b0c2d0fc1849d8fb1f
Zeridiant <jmorganbullock@gmail.com>
2017-12-02 22:46:53 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'MzU4MTAxNzE1OTg5Mjk5MjAw.DQUD1Q.hSPe3rrPBy9fyKyKpiaPKS-R95I';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = '*';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyCXpUzDyw4zohS5CcO5cNNQxQFII-Z3L3Q';
